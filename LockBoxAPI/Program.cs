b4b47cef405bb5b15faa468b0dc1d1c24736b461
David Freitas <davidjumi51@gmail.com>
2024-05-23 01:15:00 -0300

    BugFixes

--- LockBoxAPI/Program.cs
+++ LockBoxAPI/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddScoped<IVerificationEmailService, VerificationEmailService>(
 builder.Services.AddScoped<ISecurityHandler, SecurityHandler>();
 
 builder.Services.AddDbContext<LockBoxContext>(opt =>
-    opt.UseSqlServer("Server=tcp:lockboxdatabase.database.windows.net,1433;Initial Catalog=LockBoxDB;Persist Security Info=False;User ID=davidfreitas51;Password=senhaLegal123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+    opt.UseSqlServer("Server=tcp:lockboxdatabase.database.windows.net,1433;Initial Catalog=LockBoxDB;Persist Security Info=False;User ID={Your_User};Password={Your_Password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 
 
 builder.Services.AddIdentity<AppUser, IdentityRole>()
